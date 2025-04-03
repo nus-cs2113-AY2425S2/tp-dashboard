cf0ac530915410d238d93bc93c7dfd17fd359a54
Damith C. Rajapakse <1673303+damithc@users.noreply.github.com>
2025-04-04 01:17:31 +0800

    Use specific commit

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 
-./get-reposense.py --master
+./get-reposense.py --commit a0e771ee53d0cae52d244c893b134010aa4fde9e
 java -jar RepoSense.jar --config ./config --since 21/02/2025  --formats java md fxml sh bat gradle txt --timezone UTC+08 --last-modified-date
